ba913427422a136679fb2e0519af63ef38a71078
Leo <leonascim21@gmail.com>
2024-06-07 15:38:30 -0400

    Added MAUI project

---
+++ Shop.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
